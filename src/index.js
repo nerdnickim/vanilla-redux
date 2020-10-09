3a645cc28481137733f6b55efcc012a386b62eea
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-10-10 02:50:33 +0900

    create redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById("add"),
+	minus = document.getElementById("minus"),
+	number = document.querySelector("span");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const countModifier = (count = 0) => {
+	return count;
+};
+
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState());

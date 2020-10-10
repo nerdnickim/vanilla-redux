d18b442405bf2b0e7b44e07226673391735e3f31
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-10-10 16:39:42 +0900

    vanillaJs redux todoList

--- src/index.js
+++ src/index.js
@@ -1,32 +1,67 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add"),
-	minus = document.getElementById("minus"),
-	number = document.querySelector("span");
+const form = document.querySelector("form"),
+	input = form.querySelector("input"),
+	ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO",
+	DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const add_todo = (text) => {
+	return {
+		type: ADD_TODO,
+		text,
+	};
+};
 
-const countModifier = (count = 0, action) => {
+const delete_todo = (id) => {
+	return {
+		type: DELETE_TODO,
+		id,
+	};
+};
+
+const reducer = (state = [], action) => {
 	switch (action.type) {
-		case ADD:
-			return count + 1;
-		case MINUS:
-			return count - 1;
+		case ADD_TODO:
+			return [{ text: action.text, id: Date.now() }, ...state];
+		case DELETE_TODO:
+			return state.filter((toDo) => toDo.id !== action.id);
 		default:
-			return count;
+			return state;
 	}
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-	number.innerText = countStore.getState();
+const deleteTodo = (e) => {
+	const id = parseInt(e.target.parentNode.id);
+	store.dispatch(delete_todo(id));
 };
 
-countStore.subscribe(onChange);
+const paintAddList = () => {
+	const toDos = store.getState();
+	ul.innerHTML = "";
 
-add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+	toDos.forEach((toDo) => {
+		const li = document.createElement("li");
+		const btn = document.createElement("button");
+		li.id = toDo.id;
+		li.innerText = toDo.text;
+		btn.innerText = "DEL";
+		btn.addEventListener("click", deleteTodo);
+		li.appendChild(btn);
+		ul.appendChild(li);
+	});
+};
+
+store.subscribe(paintAddList);
+
+const submitHandle = (e) => {
+	e.preventDefault();
+	const toDo = input.value;
+	input.value = "";
+	store.dispatch(add_todo(toDo));
+};
+
+form.addEventListener("submit", submitHandle);

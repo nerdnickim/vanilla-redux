bab98c0bb5122d7f70fe8eff8ceca544a6b264c8
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-10-11 18:53:59 +0900

    redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,27 +1,18 @@
+import { createAction } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const ADD = "ADD",
 	DELETE = "DELETE";
 
-const addToDo = (text) => {
-		return {
-			type: ADD,
-			text,
-		};
-	},
-	deleteToDo = (id) => {
-		return {
-			type: DELETE,
-			id,
-		};
-	};
+const addToDo = createAction("ADD"),
+	deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD:
-			return [{ text: action.text, id: Date.now() }, ...state];
+			return [{ text: action.payload, id: Date.now() }, ...state];
 		case DELETE:
-			return state.filter((todo) => todo.id !== action.id);
+			return state.filter((todo) => todo.id !== action.payload);
 		default:
 			return state;
 	}
